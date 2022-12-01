9be972be0add92de6d174a68470ac6b068c11445
Amir Afshari <a_amirhossein@icloud.com>
2022-12-01 09:28:00 -0500

    finishes day 1 pt 2

--- day-1/b.ts
+++ day-1/b.ts
@@ -1,3 +1,29 @@
 export {};
 
-const input = await Deno.readTextFile("./input.txt")
+const input = await Deno.readTextFile("./input.txt");
+
+let values = input.split("\n").map((val) => Number(val));
+
+let firstIndex = 0;
+let secondIndex = 1;
+
+let firstMeasureMent = 0;
+let secondMeasurement = 0;
+
+let countIncreases = 0;
+
+while (secondIndex + 3 < values.length) {
+  for (let i = firstIndex; i < firstIndex + 3; i++) {
+    firstMeasureMent += values[i];
+  }
+  
+  for (let i = secondIndex; i < secondIndex + 3; ++i) {
+    secondMeasurement += values[i];
+  }
+  if (firstMeasureMent < secondMeasurement) ++countIncreases;
+  firstMeasureMent = secondMeasurement = 0;
+  firstIndex += 1;
+  secondIndex += 1;
+}
+
+console.log(countIncreases);
