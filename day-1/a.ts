8a7fc803f9b83b8e10dcbd3a7d9f99fa9eab6859
Amir Afshari <a_amirhossein@icloud.com>
2022-11-30 23:55:04 -0500

    init and day 1

---
+++ day-1/a.ts
@@ -0,0 +1,18 @@
+export {};
+
+const input = await Deno.readTextFile("./input.txt");
+
+let count = 0;
+
+const depths = input.split("\n").map((val) => Number(val));
+
+
+let tmp1;
+let tmp2;
+
+for(let i=0; i<depths.length; i++) {
+    
+}
+
+
+console.log(count)
